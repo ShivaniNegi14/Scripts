f3b96e664937a2ef6f07b80d0f093a714b5698e8
Ubuntu <ubuntu@ip-172-31-92-150.ec2.internal>
2023-07-23 16:46:27 +0000

    added a new file

---
+++ variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+name="AWS"
+environment="prod"
+
+echo "---------------------------"
+echo "My cloud is $name running on $environment"
+echo "********WELCOME TO $name********"
+echo "We are using $SHELL"
+echo "---------------------------"
+echo "We are at $HOME and today is "$(date)""
+
